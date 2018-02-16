045e346340ed451461daef03c4c1e6223814e042
Jack Atherton <atherton.lawrence@gmail.com>
2018-02-15 17:18:34 -0800

    laser pointer

---
+++ VR Instrument Prototypes/Assets/VR Controllers/LaserPointer.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserPointer : MonoBehaviour {
+
+    private SteamVR_TrackedObject trackedObj;
+    private SteamVR_Controller.Device Controller
+    {
+        get { return SteamVR_Controller.Input((int)trackedObj.index); }
+    }
+
+    private void Awake()
+    {
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+    }
+
+    // laser!
+    public GameObject laserPrefab;
+    private GameObject laser;
+    private Transform laserTransform;
+    private Vector3 hitPoint; 
+
+	private void ShowLaser(RaycastHit hit)
+    {
+        laser.SetActive(true);
+        laserTransform.position = Vector3.Lerp(trackedObj.transform.position, hitPoint, .5f);
+        laserTransform.LookAt(hitPoint); 
+        laserTransform.localScale = new Vector3(laserTransform.localScale.x, laserTransform.localScale.y,
+            hit.distance);
+    }
+
+    private void HideLaser()
+    {
+        laser.SetActive( false );
+    }
+
+    private void Start()
+    {
+        laser = Instantiate(laserPrefab);
+        laserTransform = laser.transform;
+    }
+
+    private void Update()
+    {
+        if (Controller.GetPress(SteamVR_Controller.ButtonMask.Touchpad))
+        {
+            RaycastHit hit;
+
+            if (Physics.Raycast(trackedObj.transform.position, transform.forward, out hit, 400))
+            {
+                hitPoint = hit.point;
+                ShowLaser(hit);
+            }
+            else
+            {
+                HideLaser();
+            }
+        }
+        else 
+        {
+            HideLaser();
+        }
+    }
+}
