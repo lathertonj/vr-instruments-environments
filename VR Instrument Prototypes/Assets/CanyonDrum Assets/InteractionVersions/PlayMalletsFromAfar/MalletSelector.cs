22de4d7f98d075c35332867990de7108eaab0d9d
Jack Atherton <atherton.lawrence@gmail.com>
2018-02-28 11:46:44 -0800

    add laser selection for the mallets

--- VR Instrument Prototypes/Assets/CanyonDrum Assets/InteractionVersions/PlayMalletsFromAfar/MalletSelector.cs
+++ VR Instrument Prototypes/Assets/CanyonDrum Assets/InteractionVersions/PlayMalletsFromAfar/MalletSelector.cs
@@ -21,18 +21,59 @@ public class MalletSelector : MonoBehaviour , ColliderBridgeListener
 
 
     // todo: refactor to private + selector
-    public Rigidbody myMallet;
-    public Transform myMalletPosition;
+    private Rigidbody myMallet;
+    // public Transform myMalletPosition;
 
+    private LaserPointer myLaserPointer;
 
     private void Start()
     {
-        // set up link to receive haptic feedback on collision
-        myMallet.GetComponent<ColliderBridgeSource>().Initialize( this );
+        // set up laser pointer to show laser when thumb is resting on trackpad
+	    myLaserPointer = GetComponent<LaserPointer>();
+        // don't need to press the button to show the laser
+        myLaserPointer.laserOnTouchpadButtonPress = false;
+	}
+	
+	void LateUpdate () {
+	    if( Controller.GetPressDown(SteamVR_Controller.ButtonMask.Touchpad) )
+        {
+            Collider collidingObject = myLaserPointer.GetFoundCollider();
+            // if we collided with something and it has the Mallet tag:
+            if( collidingObject != null && collidingObject.CompareTag( "Mallet" ) )
+            {
+                // stop controlling whatever mallet I have now
+                StopControllingMallet(); 
+
+                // control this mallet now
+                myMallet = collidingObject.GetComponentInParent<Rigidbody>();
+                // set up link to receive haptic feedback on collision
+                myMallet.GetComponent<ColliderBridgeSource>().Initialize( this );
+            }
+            else
+            {
+                StopControllingMallet();
+            } 
+        }
+	}
+
+    private void StopControllingMallet()
+    {
+        // no longer control whatever mallet we happen to have now
+        if( myMallet != null )
+        {
+            // unset haptic feedback link
+            myMallet.GetComponent<ColliderBridgeSource>().Initialize( null );
+        }
+        // delete my storage reference
+        myMallet = null;
     }
 
     private void FixedUpdate()
     {
+        // if not controlling a mallet, do nothing
+        if( myMallet == null ) { return ; }
+
+
         // 1. Set velocity equivalent. Too insensitive but works a tiny amount!
         // myMallet.velocity = Controller.velocity;
         
@@ -57,6 +98,9 @@ public class MalletSelector : MonoBehaviour , ColliderBridgeListener
         // myMallet.angularVelocity = -20 * Controller.angularVelocity;
     }
 
+
+    // ======================== HAPTIC FEEDBACK ============================ //
+    // Implement BridgeCollisionEnter
     void ColliderBridgeListener.BridgeCollisionEnter( Collision collision )
     {
         // on a collision, cause a haptic feedback pulse
