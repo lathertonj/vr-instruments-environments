8f7b534b3bcb00bb831fb650622efbeeff698624
Jack Atherton <atherton.lawrence@gmail.com>
2018-04-25 16:24:00 -0700

    make bubbles float up and down (breaks sound sometimes, so turned off for now)

---
+++ VR Instrument Prototypes/Assets/FlyingAssets/BubbleFloat.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BubbleFloat : MonoBehaviour
+{
+    float myFloatAmount;
+    float myFloatRate;
+    Vector3 myStartPos;
+
+	// Use this for initialization
+	void Start ()
+    {
+	    myStartPos = transform.position;
+        myFloatAmount = Random.Range( 0.01f, 0.08f );
+        myFloatRate = Random.Range( 0.05f, 0.5f );
+	}
+	
+	// Update is called once per frame
+	void Update()
+    {
+        transform.position = myStartPos + Vector3.up * myFloatAmount * Mathf.Sin( Mathf.PI * myFloatRate * Time.time );
+	}
+}
