b4c82c139e01a5d29f05e6ae8f2e8bf61b90ff35
Jack Atherton <atherton.lawrence@gmail.com>
2018-05-18 14:22:10 -0700

    room full of ohno bots!

--- VR Instrument Prototypes/Assets/OhnoBots Assets/OhNoBotController.cs
+++ VR Instrument Prototypes/Assets/OhnoBots Assets/OhNoBotController.cs
@@ -9,12 +9,16 @@ public class OhNoBotController : MonoBehaviour
     Rigidbody myRB;
     bool amTouchingGround = false;
     Vector3 forceToAdd;
+    Renderer[] myRenderers;
+    Color origColor;
 
     // Use this for initialization
     void Start()
     {
         myChuck = GetComponent<ChuckSubInstance>();
         myRB = GetComponent<Rigidbody>();
+        myRenderers = GetComponentsInChildren<Renderer>();
+        origColor = myRenderers[0].material.color;
     }
 
     private void FixedUpdate()
@@ -26,32 +30,72 @@ public class OhNoBotController : MonoBehaviour
         }
     }
 
-    private void OnTriggerEnter( Collider other )
-    {
-        // When a controller hits me, jump back
-        if( amTouchingGround && other.gameObject.CompareTag( "Controller" ) )
-        {
-            string filename = Random.Range( 0f, 1f ) < 0.5f ? "oops.wav" : "ohno.wav";
-            float rate = Random.Range( 1.5f, 2.5f );
-            myChuck.RunCode( string.Format(
-            @"
-                SndBuf buf => dac;
-                me.dir() + ""{0}"" => buf.read;
-                {1} => buf.rate;
-                buf.length() / buf.rate() => now;
-            
-            ", filename, rate
-            ) );
 
-            // away from the thing I collided with
-            Vector3 jumpDirection = ( transform.position - other.transform.position ).normalized;
-            // but with a certain up direction
-            jumpDirection.y = 0.5f;
-            // renormalize
-            jumpDirection.Normalize();
-            forceToAdd = jumpDirection * jumpForce;
+
+    private void ReactToObject( GameObject other )
+    {
+        // Only react if I am on the ground
+        if( !amTouchingGround )
+        {
+            return;
         }
 
+        // Only react to VR controllers, other bots, and projectiles
+        if( !other.CompareTag( "Controller" ) &&
+            !other.CompareTag( "Bot" ) &&
+            !other.CompareTag( "Throwable" ) )
+        {
+            return;
+        }
+
+        // When something hits me, jump back
+        string filename = Random.Range( 0f, 1f ) < 0.5f ? "oops.wav" : "ohno.wav";
+        float rate = Random.Range( 1.5f, 2.5f );
+        myChuck.RunCode( string.Format( @"
+            SndBuf buf => dac;
+            me.dir() + ""{0}"" => buf.read;
+            {1} => buf.rate;
+            buf.length() / buf.rate() => now;
+            
+            ", filename, rate
+        ) );
+
+        // away from the thing I collided with
+        Vector3 jumpDirection = ( transform.position - other.transform.position ).normalized;
+        // but with a certain up direction
+        jumpDirection.y = 0.5f;
+        // renormalize
+        jumpDirection.Normalize();
+        forceToAdd = jumpDirection * jumpForce;
+
+        // show an animation
+        StartCoroutine( "FadeColor" );
+    }
+
+    IEnumerator FadeColor()
+    {
+        Color tempColor = Color.magenta;
+        for( float f = 1f; f >= 0; f -= 0.05f )
+        {
+            SetColor( origColor + f * ( tempColor - origColor ) );
+            yield return null;
+        }
+
+        // set back to normal
+        SetColor( origColor );
+    }
+
+    private void SetColor( Color c )
+    {
+        for( int i = 0; i < myRenderers.Length; i++ )
+        {
+            myRenderers[i].material.color = c;
+        }
+    }
+
+    private void OnTriggerEnter( Collider other )
+    {
+        ReactToObject( other.gameObject );
     }
 
     private void OnCollisionEnter( Collision collision )
@@ -61,6 +105,9 @@ public class OhNoBotController : MonoBehaviour
         {
             amTouchingGround = true;
         }
+
+        // maybe react
+        ReactToObject( collision.gameObject );
     }
 
     private void OnCollisionStay( Collision collision )
---
+++ VR Instrument Prototypes/Assets/OhnoBots Assets/OhNoBotGenerator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OhNoBotGenerator : MonoBehaviour
+{
+    public Transform ohnoBotPrefab;
+    public int numBots;
+
+    // Use this for initialization
+    void Start()
+    {
+        for( int i = 0; i < numBots; i++ )
+        {
+            Vector3 newPos = new Vector3(
+                Random.Range( -1f, 1f ),
+                0.1f,
+                Random.Range( -1f, 1f )
+            );
+
+            Instantiate( ohnoBotPrefab, newPos, Quaternion.identity, transform );
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
